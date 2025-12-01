cc020ca6fbd840ca5af760102aa14f041e735e2c
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-01 22:26:30 +0530

    AI with Python

---
+++ types.py
@@ -0,0 +1,12 @@
+kunal_house = 12
+lucky_house = "jaat land"
+print(type(kunal_house))
+
+adsress = str(kunal_house) +   lucky_house 
+print(adsress)
+
+
+w = len("my_string")
+print(type(w))
+g = "hippo" *12
+print(type(g))
