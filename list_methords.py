7244f1e3bded2af9c09f72d1289abebbe1a5fd0d
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-03 21:05:40 +0530

    Data Structure in Python

---
+++ list_methords.py
@@ -0,0 +1,9 @@
+list1 = [34,43345,56,46,46565,432,25,5]
+print(max(list1))
+
+list2 = ["kaunl_amlik", "lucky_malik", "jaat_singh", "zhaudharycharan_singh"]
+print(min(list2))
+
+list3 = ["kunal.malik", "lucky.malik",23,23,13134]
+print(max(list3))
+
