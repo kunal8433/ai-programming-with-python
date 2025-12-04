cbd4da34752da74bbd4587681f75a369a21daa7b
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-04 22:07:14 +0530

    Control Flow

---
+++ for_loops.py
@@ -0,0 +1,12 @@
+countary = [
+    "india",
+       "usa",
+           "china",   
+                 "us"
+    
+    ]
+while True:
+    for country in countary:
+        print(country)
+    break
+    
