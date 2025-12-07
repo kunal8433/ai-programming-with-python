07a4741a4df6b14ff352fdec3c702237134ff945
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-07 10:27:21 +0530

    Scripting

---
+++ try.py
@@ -0,0 +1,10 @@
+def main():
+      try:
+          x = int(input("Enter a number:"))
+          print(x)
+          return
+      except:
+         print("write an valid code")
+
+ 
+      print("Plss enter an valid number")
