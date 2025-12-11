48ec1dbc8fca4056a26efbe0592b0c31f3055c2c
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-12 00:20:34 +0530

    oops

---
+++ self.py
@@ -0,0 +1,12 @@
+class Employees  :
+    language = "python"
+    Salary = 1200000
+
+    def wish(self):
+        print("GM")
+    def getInfo (self):
+        print(f"the language is {self.language}, the salary is {self.Salary}")
+
+kunal = Employees ()
+kunal.wish()
+kunal.getInfo()
