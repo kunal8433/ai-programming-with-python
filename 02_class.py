66893fe8aade5ccb26fe034335a1d5385360c6c2
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-12 00:19:49 +0530

    oops

---
+++ 02_class.py
@@ -0,0 +1,12 @@
+class Employees  :
+    language = "python"
+    Salary = 1200000
+
+kunal = Employees ()
+kunal.name = "Kunal Jaat"
+print(kunal.name,kunal. language,kunal. Salary)
+lucky = Employees()
+lucky.name = "Lucky jaat"
+print("Employees name is :", lucky.name)
+print("Language used:" ,lucky.language )
+print("Salary is:" , lucky.Salary)
