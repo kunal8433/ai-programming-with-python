a41f8fb82dc81edc643b77380a5afd079be6d042
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-28 22:18:04 +0530

    opps

---
+++ 01_revesion.py
@@ -0,0 +1,12 @@
+class programmer :
+    company = "Microsoft"
+    def __init__(self,name,age,salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+kunal = programmer("kuanl", 17 , 12000000)
+print("Name of programmer is :",kunal.name)
+print()
+print("Age of programmer is :",kunal.age)
+print()
+print("Salary oof programmer is :",kunal.salary)
