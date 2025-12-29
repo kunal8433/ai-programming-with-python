2bdc9807c913b9048f2317307f543e8611bb06f2
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-29 19:56:57 +0530

    OPPS

---
+++ 05_revesion.py
@@ -0,0 +1,15 @@
+class calculator:
+    def __init__ (self,n):
+        self.n  = n
+    
+    def square (self):
+        print(f"The square is:{self.n*self.n}")
+    def cube (self):
+        print(f"The cube is:{self.n*self.n*self.n}")
+    def squareroot (self):
+        print(f"The squareroot is:{self.n**1/2}")
+kunal = calculator(3)
+kunal.square()
+kunal.cube()
+kunal.squareroot()
+             
