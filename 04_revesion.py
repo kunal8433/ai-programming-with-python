415915bbba37e7e814216b4e826be27bcd50c29e
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-12-29 19:56:32 +0530

    OPPS

---
+++ 04_revesion.py
@@ -0,0 +1,7 @@
+class programmer :
+    company = "Microsoft"
+    def __init__(self,name,pincode):
+        self.name = name
+        self.pincode = pincode
+kunal = programmer("kunal",250103)
+print(f"Name of Programmer is : {kunal.name}/n Pincode is : {kunal.pincode}")
