3bb025b80f5f5763b2a13731b13ea773a7741eb8
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-07 20:33:16 +0530

    NumPy

---
+++ 20_.py
@@ -0,0 +1,16 @@
+import numpy as np 
+x = np.array([1,2,3,4,5])
+y = np.array([6,7,8,9,10])
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**2)
+print(x%2)
+print(np.add(x,y))
+print(np.subtract(x,y))
+print(np.multiply(x,y))
+
+print(np.divide(x,y))
+print(np.power(x,2))
