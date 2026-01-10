2d88b5628011907dc5588e4ffe737de2ae961172
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-10 16:09:48 +0530

    Pandas

--- 10_.py
+++ 10_.py
@@ -1,4 +1,5 @@
-import numpy as np
-x = np.full((3,4), 5)
-print(x)
-print(x.dtype)
+import pandas as pd
+items2 = [{'bikes': 20, 'pants': 30, 'watches': 35}, 
+          {'watches': 10, 'glasses': 50, 'bikes': 15, 'pants':5}]
+items = pd.DataFrame(items2)
+print(items)
