68c38f506ccc4a37c4a1267e33155f613b50da74
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-10 16:08:48 +0530

    Pandas

--- 08_.py
+++ 08_.py
@@ -1,5 +1,13 @@
-import numpy as np
-x = np.ones((3,4))
-print(x)
-print(type(x))
-print(x.dtype)
+import pandas as pd
+items = {"kunal" :pd.Series([10,20,30,40], index = ["book", "pen", "notebook", "bag"]),
+         "lucky": pd.Series ([15,25,35,45], index = ["book", "pen", "notebook", "bag"]),}
+print(type(items))
+df = pd.DataFrame(items)
+print(df)
+
+print(items.values)
+data = {'Alice': pd.Series([40, 110, 500, 45]),
+        'Bob': pd.Series([245, 25, 55])}
+
+df = pd.DataFrame(data)
+print(df)
