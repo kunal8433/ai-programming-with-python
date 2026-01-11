2e16d924b2ed20649a7fbaf61817a35340857fbc
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-11 16:00:44 +0530

    Pandas

--- 14_.py
+++ 14_.py
@@ -1,4 +1,9 @@
-import numpy as np 
-z = np.arange(10,50)
-x = np.reshape( 0, 15,5)
+import pandas as pd
+items2 = [{'bikes': 20, 'pants': 30, 'watches': 35}, 
+          {'watches': 10, 'glasses': 50, 'bikes': 15, 'pants':5}]
+items = pd.DataFrame(items2, index = ["store1", "store2"])
+
+x = items.isnull()#.sum().sum()
 print(x)
+y = items.count()
+print(y)
