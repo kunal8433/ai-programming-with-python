986a305cf8cb5c85e8ae0b492f15feeb1a45a235
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-14 14:09:24 +0530

    Pandas

--- 04_.py
+++ 04_.py
@@ -1,23 +1,11 @@
-# TO DO: Necessary import
+import pandas as pd
+data = {"name": ["kunalmalik", "lucky malik"] , "marks":[98,99]}
 
-# Read the CSV file
-fuel_econ = pd.read_csv('fuel_econ.csv')
-fuel_econ.head(10)
-
-
-plt.figure(figsize = [18, 6])
-
-# PLOT ON LEFT
-plt.subplot(1, 2, 1)
-sb.regplot(data = fuel_econ, x = 'displ', y = 'comb', x_jitter=0.04, scatter_kws={'alpha':1/10}, fit_reg=False)
-plt.xlabel('Displacement (1)')
-plt.ylabel('Combined Fuel Eff. (mpg)');
-
-# PLOT ON RIGHT
-plt.subplot(1, 2, 2)
-plt.hist2d(data = fuel_econ, x = 'displ', y = 'comb')
-plt.colorbar()
-plt.xlabel('Displacement (1)')
-plt.ylabel('Combined Fuel Eff. (mpg)');import pandas as pd
-import matplotlib.pyplot as plt 
-import seaborn as sb
+df = pd.DataFrame(data)
+print(df)
+print(df.head())
+print(df.tail())
+print(df.describe())
+print(df.info())
+print(df.shape)
+print(df.isnull())
