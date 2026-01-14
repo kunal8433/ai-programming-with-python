2b7c474127a4f792f5b73a5c89b36465804da652
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-14 14:08:30 +0530

    Pandas

--- 01_.py
+++ 01_.py
@@ -1,12 +1,4 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
-
-pokemon = pd.read_csv(r'D:\Matpotlib\pokemon.csv')
-
-print(pokemon.shape)
-print(pokemon.head(10))
-
-sb.countplot(data=pokemon, x='generation_id')
-plt.show()
+data = {"name": ["kunalmalik", "lucky malik"] , "marks":[98,99]}
+ds = pd.Series(data)
+print(ds)
